695006d087ec57491b78d045999a78c3d9c65952
ElqBell <silenc8@gmail.com>
2020-04-15 15:41:31 +0300

    uploading first files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
