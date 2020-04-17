c78f78098418f1cc0fc2e284e2f1060c126c803d
ElqBell <silenc8@gmail.com>
2020-04-18 00:31:41 +0300

    created template (to be changed later) components: Dashboard, Notifications, ProjectList

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ProjectList = (props) => {
+    return (
+        <div className="project-list sections">
+            <div className="card z-depth-0 project-summary">
+                <div className="card-conent grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by DMD</p>
+                    <p className="grey-text">11th August, 11:03AM</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-conent grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by DMD</p>
+                    <p className="grey-text">11th August, 11:03AM</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-conent grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by DMD</p>
+                    <p className="grey-text">11th August, 11:03AM</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectList;
