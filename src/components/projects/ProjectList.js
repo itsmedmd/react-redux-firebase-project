ffe4c07c966f94920d35e1d665d7c0d1faac28d9
ElqBell <silenc8@gmail.com>
2020-04-18 00:35:39 +0300

    created ProjectSummary component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,31 +1,13 @@
 import React from 'react';
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = (props) => {
     return (
         <div className="project-list sections">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-conent grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by DMD</p>
-                    <p className="grey-text">11th August, 11:03AM</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-conent grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by DMD</p>
-                    <p className="grey-text">11th August, 11:03AM</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-conent grey-text text-darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by DMD</p>
-                    <p className="grey-text">11th August, 11:03AM</p>
-                </div>
-            </div>
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
         </div>
     )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = (props) => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-conent grey-text text-darken-3">
+                <span className="card-title">Project Title</span>
+                <p>Posted by DMD</p>
+                <p className="grey-text">11th August, 11:03AM</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary;
