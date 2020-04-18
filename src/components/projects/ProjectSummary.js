6ac7b33082c7524383caff459f8560fa14426d73
ElqBell <silenc8@gmail.com>
2020-04-19 01:50:27 +0300

    created dummy data

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ProjectSummary = (props) => {
+const ProjectSummary = ({ project }) => {
     return (
         <div className="card z-depth-0 project-summary">
             <div className="card-conent grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
+                <span className="card-title">{project.title}</span>
                 <p>Posted by DMD</p>
                 <p className="grey-text">11th August, 11:03AM</p>
             </div>
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+    projects: [
+        {id: '1', title: 'party starting boys', content: 'party we good!'},
+        {id: '2', title: 'goin to shoot some hogs', content: 'yee haw KKona!'},
+        {id: '3', title: 'AYAYA', content: 'japan so cool xd!'}
+    ]
 };
 
 const projectReducer = (state = initState, action) => {
